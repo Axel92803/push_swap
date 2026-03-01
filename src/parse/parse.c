be6b95970a625cfd97c5c6a9ad43c6661baa2d80
Alex <ionut.a.tanvuia@gmail.com>
2026-03-01 17:20:45 +0000

    feat(parse): implement strong parsing engine to account for all edge cases

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,5 +1,29 @@
 #include "../../inc/push_swap.h"
 
+static char	***split_arg(int argc, char **argv)
+{
+	char	***args;
+	int		i;
+	int		flag;
+	t_strategy	dummy;
+
+	i = 0;
+	flag = parse_arg(argc, argv, &dummy);
+	args = (char ***)malloc(sizeof(char **) * (argc - flag + 1));
+	if (!args)
+		return (NULL);
+	while (((argc >= 3 && flag == 2) || (argc >= 2 && flag == 1)) && flag < argc)
+	{
+		args[i] = ft_split(argv[flag], ' ');
+		if (!args[i])
+			return (free_args(args, i), NULL);
+		i++;
+		flag++;
+	}
+	args[i] = NULL;
+	return (args);
+}
+
 int	parse_arg(int argc, char **argv, t_strategy *strategy)
 {
 	int	i;
@@ -22,23 +46,31 @@ int	parse_arg(int argc, char **argv, t_strategy *strategy)
 	return (i);
 }
 
-int	parse_num(int argc, char **argv, int start, t_node a)
+int	parse_num(int argc, char **argv, t_node **a)
 {
 
 	int	i;
+	int j;
 	long	num;
+	char	***args;
 
-	i = start;
+	i = 0;
+	args = split_arg(argc, argv);
 	while(i < argc)
 	{
-		if (!syntax_err(argv[i]))
-			free_err(a);
-		num = ft_atol(argv[i]);
-		if (num < INT_MIN || num > INT_MAX)
-			free_err(a);
-		if (dup_err(a, (int)num))
-			free_err(a);
-		append_node(a, (int)num);
+		j = 0;
+		while(args[i][j])
+		{
+			if (!syntax_err(argv[i][j]))
+				free_err(a);
+			num = ft_atol(argv[i][j]);
+			if (num < INT_MIN || num > INT_MAX)
+				free_err(a);
+			if (!dup_err(a, (int)num))
+				free_err(a);
+			append_node(a, (int)num);
+			j++;
+		}
 		i++;
 	}
 }
