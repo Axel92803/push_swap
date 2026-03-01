4e110acdfe122a46c968a19e8d13e95bd9909dbe
Alex <ionut.a.tanvuia@gmail.com>
2026-03-01 22:03:14 +0000

    feat(init): add function to append new node and create stack a in parse_num()

---
+++ src/init/stack_init.c
@@ -0,0 +1,25 @@
+#include "../../inc/push_swap.h"
+
+void	append_node(t_node **a, int num)
+{
+	t_node	*new;
+
+	new = (t_node *)malloc(sizeof(t_node));
+	if (!new)
+		free_err(a);
+	new->val = num;
+	if (!*a)
+	{
+		*a = new;
+		(*a)->next = *a;
+		(*a)->prev = *a;
+	}
+	else
+	{
+		new->prev = (*a)->prev;
+		(*a)->prev->next = new;
+		new->next = *a;
+		(*a)->prev = new;
+	}
+}
+
