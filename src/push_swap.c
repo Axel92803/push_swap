2609507a011771d653507e9ca30e5aa6169f6e0e
Ionut Tanvuia <itanvuia@c1r1s4.42london.com>
2026-03-03 12:52:32 +0000

    fix(main): fix memory leak in main() with free_stack() and clean redundant function calls in disorder()

--- src/push_swap.c
+++ src/push_swap.c
@@ -2,25 +2,29 @@
 
 static double gargantua(int argc, char **argv, t_node **a, t_strategy *flag)
 {
-	char ***args;
+	char	***args;
 	int		*flat_array;
 	int		size;
 	double	dis;
+	int		arg_n;
 
 	args = split_arg(argc, argv);
 	size = count_args(args);
 	flat_array = parse_num(args, a);
-	parse_arg(argc, argv, flag);
+	arg_n = parse_arg(argc, argv, flag);
 	dis = disorder_calc(flat_array, size);
 
 	free(flat_array);
-	free_args(args, argc - parse_arg(argc, argv, flag));
+	free_args(args, argc - arg_n);
 
 	return (dis);
 }
 
 static void	selector(t_node	**a, t_node	**b, double	dis, t_strategy flag)
 {
+	(void)a;
+	(void)b;
+
 	if (flag == ADAPTIVE)
 	{
 		if (dis < 0.2)
@@ -31,11 +35,11 @@ static void	selector(t_node	**a, t_node	**b, double	dis, t_strategy flag)
 			flag = COMPLEX;
 	}
 	if (flag == SIMPLE)
-		sort_simple(a, b);
+		return ;	//sort_simple(a, b);
 	else if (flag == MEDIUM)
-		sort_medium(a, b);
+		return ;	//sort_medium(a, b);
 	else if (flag == COMPLEX)
-		sort_complex(a, b);
+		return ;	//sort_complex(a, b);
 }
 
 int	main(int argc, char **argv)
@@ -51,6 +55,7 @@ int	main(int argc, char **argv)
 	b = NULL;
 	dis = gargantua(argc, argv, &a, &flag);
 	selector(&a, &b, dis, flag);
+	free_stack(&a);
 	return (0);
 }
 
